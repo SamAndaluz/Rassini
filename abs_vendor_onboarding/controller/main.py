04d84a38e67b4d22accdc0f83c9cb0b30e34edca
Bhaumin <bhaumin.chorera@gmail.com>
2020-02-08 10:57:29 +0530

    [ADD]Added new interface on Onboarding forms.

--- abs_vendor_onboarding/controller/main.py
+++ abs_vendor_onboarding/controller/main.py
@@ -12,6 +12,19 @@ from odoo.http import Controller, request, route
 
 class WebsiteOnboardingForm(http.Controller):
 
+    def _prepare_portal_layout_values(self):
+        # get customer sales rep
+        sales_user = False
+        partner = request.env.user.partner_id
+        if partner.user_id and not partner.user_id._is_public():
+            sales_user = partner.user_id
+
+        return {
+            'sales_user': sales_user,
+            'page_name': 'home',
+            'archive_groups': [],
+        }
+
     @http.route('/company_detail/submit', type="http", auth="user", website=True)
     def form_company_detail_submit(self, **post):
         partner = request.env.user.partner_id
@@ -20,25 +33,87 @@ class WebsiteOnboardingForm(http.Controller):
             return http.request.render('abs_vendor_onboarding.menu_onboarding')
 
     @http.route('/fiscal_address', type="http", auth="user", website=True)
-    def form_fiscal_address(self, **kwargs):
+    def form_fiscal_address(self, redirect=None, **post):
+        values = self._prepare_portal_layout_values()
         partner = request.env.user.partner_id
-        value = {}
-        values = {}
-        value = kwargs
-        state = 'state_id' in value and value['state_id'] != '' and request.env['res.country.state'].browse(int(value['state_id']))
-        state = state and state.exists()
-        state_province_ids = request.env['res.country.state'].sudo().search([])        
+        values.update({
+            'error': {},
+            'error_message': [],
+        })
 
-        country = 'country_id' in value and value['country_id'] != '' and request.env['res.country'].browse(int(value['country_id']))
-        country = country and country.exists()
-        country_ids = request.env['res.country'].sudo().search([])
-        values = {
-                  'country'                : country,
-                  'country_ids'            : country_ids,
-                  'state'                  : state,
-                  'state_province_ids'     : state_province_ids,
-                 }
-        return http.request.render('abs_vendor_onboarding.form_fiscal_address', values)
+        if post and request.httprequest.method == 'POST':
+            error, error_message = self.details_form_validate(post)
+            values.update({'error': error, 'error_message': error_message})
+            values.update(post)
+            if not error:
+                values = {key: post[key] for key in self.MANDATORY_BILLING_FIELDS}
+                values.update({key: post[key] for key in self.OPTIONAL_BILLING_FIELDS if key in post})
+                values.update({'country_id': int(values.pop('country_id', 0))})
+                values.update({'zip': values.pop('zipcode', '')})
+                if values.get('state_id') == '':
+                    values.update({'state_id': False})
+                partner.sudo().write(values)
+                if redirect:
+                    return request.redirect(redirect)
+                return request.redirect('/my/home')
+
+        countries = request.env['res.country'].sudo().search([])
+        states = request.env['res.country.state'].sudo().search([])
+
+        values.update({
+            'partner': partner,
+            'countries': countries,
+            'states': states,
+            'commercial_name' : partner.company_name,
+            'has_check_vat': hasattr(request.env['res.partner'], 'check_vat'),
+            'redirect': redirect,
+            'page_name': 'my_details',
+        })
+
+        response = request.render("abs_vendor_onboarding.form_fiscal_address", values)
+        response.headers['X-Frame-Options'] = 'DENY'
+        return response
+
+    @http.route('/company_detail', type="http", auth="user", website=True)
+    def form_company_detail(self, redirect=None, **post):
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        values.update({
+            'error': {},
+            'error_message': [],
+        })
+
+        if post and request.httprequest.method == 'POST':
+            error, error_message = self.details_form_validate(post)
+            values.update({'error': error, 'error_message': error_message})
+            values.update(post)
+            if not error:
+                values = {key: post[key] for key in self.MANDATORY_BILLING_FIELDS}
+                values.update({key: post[key] for key in self.OPTIONAL_BILLING_FIELDS if key in post})
+                values.update({'country_id': int(values.pop('country_id', 0))})
+                values.update({'zip': values.pop('zipcode', '')})
+                if values.get('state_id') == '':
+                    values.update({'state_id': False})
+                partner.sudo().write(values)
+                if redirect:
+                    return request.redirect(redirect)
+                return request.redirect('/my/home')
+
+        countries = request.env['res.country'].sudo().search([])
+        states = request.env['res.country.state'].sudo().search([])
+
+        values.update({
+            'partner': partner,
+            'countries': countries,
+            'states': states,
+            'has_check_vat': hasattr(request.env['res.partner'], 'check_vat'),
+            'redirect': redirect,
+            'page_name': 'my_details',
+        })
+
+        response = request.render("abs_vendor_onboarding.form_company_detail_new", values)
+        response.headers['X-Frame-Options'] = 'DENY'
+        return response
 
     @http.route('/fiscal_address/submit', type="http", auth="public", website=True)
     def form_fiscal_address_submit(self, **post):
